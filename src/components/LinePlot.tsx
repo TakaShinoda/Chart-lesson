68b867f5e369ce05189f93d6bc24058a6bceff10
Takahiro <tkhr_621@docomo.ne.jp>
2020-08-23 23:56:04 +0900

    ダミーの折れ線グラフのコンポーネントを作成

---
+++ src/components/LinePlot.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import { Line } from 'react-chartjs-2'
+
+const data = {
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  datasets: [
+    {
+      label: 'Demo line plot', // グラフの名前
+      backgroundColor: '#008080',
+      borderColor: '#7fffd4',
+      pointBorderWidth: 10,
+      data: [5, 6, 9, 15, 30, 40, 80], //labelsの値
+    },
+  ],
+}
+
+export const LinePlot: FC = () => {
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  )
+}
