da9838df310e3f1e563ae5fac33d18390282c07d
Takahiro <tkhr_621@docomo.ne.jp>
2020-08-26 01:18:46 +0900

    データを追加

--- src/components/RadarPlot.tsx
+++ src/components/RadarPlot.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { Radar } from 'react-chartjs-2'
 
 const data = {
-  labels: ['React', 'React', 'React', 'React', 'React', 'React'],
+  labels: ['React', 'Vue', 'Angular', 'JavaScript', 'TypeScript', 'Redux', 'REST API'],
   datasets: [
     {
       label: 'Person A',
@@ -12,6 +12,14 @@ const data = {
       pointBoederColor: '#fff',
       data: [100, 50, 30, 90, 50, 70, 40],
     },
+    {
+        label: 'Person B',
+        backgroundColor: 'rgba(179,181,198,0.2)',
+        borderColor: '#ff1493',
+        pointBackgroundColor: '#ff1493',
+        pointBoederColor: '#fff',
+        data: [50, 30, 100, 10, 100, 70, 60],
+      },
   ],
 }
 
